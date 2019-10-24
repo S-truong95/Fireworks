bb82f4405949faf476f66aafde0a7db9245fbde7
Steven <steventruong1995@yahoo.com>
2019-10-24 10:56:20 -0400

    MVP

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import main from "./js/main"
 
 main()
+
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,11 @@
+import Fireworks from "fireworks-canvas"
+
 export default () =>{
     alert("It works !")
 }
+
+export default () => {
+    const app = document.getElementById("app")
+    const fireworks = new Fireworks(app)
+    fireworks.start()
+  }
